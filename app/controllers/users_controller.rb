36946ea7f30e74c5c2dfa0b604419f71e2265eb7
Anoob Bava <anoob.bava@gmail.com>
2018-05-28 17:14:23 +0530

    completed CRUD operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+
+  def new
+    # binding.pry
+    @user = UserInformation.new
+  end
+
+  def index
+    @users = UserInformation.all
+  end
+
+  def create
+    @user = UserInformation.new(permit_params)
+    if @user.save
+      flash[:notice] = 'User information created'
+      redirect_to :root
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def permit_params
+    params.require(:user_information).permit(:first_name, :last_name, :secret_message)
+  end
+
 end
